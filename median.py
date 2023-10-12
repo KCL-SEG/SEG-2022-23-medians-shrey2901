b7b48c8e2e5e54119cc76bad408f38e3d4daeff1
Shrey <shreyansh290103@outlook.com>
2023-10-12 03:48:48 +0100

    Caculate median from a list
    Made a function to calculate the median for modularity. Note: Method call is in the print statement

--- median.py
+++ median.py
@@ -1,5 +1,13 @@
-"""Median calculator."""
-"""ENTER YOUR SOLUTION HERE!"""
+"'@version 1.2 @author Shrey'"
+""'create method to calculate the median for modularity "'
+"'Used method call in the prinitng statement '"
+
+def median(numbers):
+    if len(numbers) % 2:
+        return numbers[int(len(numbers)/2)]
+    else:
+        index = int(len(numbers)/2)
+        return (numbers[index-1]+numbers[index])/2
 
 while True:
     try:
@@ -9,4 +17,4 @@ while True:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+print(f"The median is : {median(numbers)}")
